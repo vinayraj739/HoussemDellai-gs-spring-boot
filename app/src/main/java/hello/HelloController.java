e326388b46592e95c82d9c6e2974927e448054e4
vinayraj739 <140806587+vinayraj739@users.noreply.github.com>
2023-08-10 16:42:25 +0530

    commit 2

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot vinay";
+        return "Hello from Spring Boot vinayraj";
     }   
 }
